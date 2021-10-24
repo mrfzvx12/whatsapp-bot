6fdac1e1e05bcaa144780ffed005a112b1e1b1ee
Ma'ruf <72728486+mrfzvx12@users.noreply.github.com>
2021-10-24 12:04:43 +0700

    Add files via upload

---
+++ functions/directory.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+let direc = JSON.parse(fs.readFileSync('./database/directory.json'));
+
+/**
+ * add nama file image yang tersimpan di dalam database 
+ * @param { string } value
+*/
+const addImage = (value) => {
+  text = value;
+  direc.image.push(text);
+  fs.writeFileSync('./database/directory.json', JSON.stringify(direc, null, '\t'));
+};
+
+/**
+ * add nama file video yang tersimpan di dalam database 
+ * @param { string } value
+*/
+const addVideo = (value) => {
+  text = value;
+  direc.video.push(text);
+  fs.writeFileSync('./database/directory.json', JSON.stringify(direc, null, '\t'));
+};
+
+/**
+ * add nama file audio yang tersimpan di dalam database 
+ * @param { string } value
+*/
+const addAudio = (value) => {
+  text = value;
+  direc.audio.push(text);
+  fs.writeFileSync('./database/directory.json', JSON.stringify(direc, null, '\t'));
+};
+
+/**
+ * add nama file stiker yang tersimpan di dalam database 
+ * @param { string } value
+*/
+const addStiker = (value) => {
+  text = value;
+  direc.sticker .push(text);
+  fs.writeFileSync('./database/directory.json', JSON.stringify(direc, null, '\t'));
+};
+
+module.exports = {
+  direc,
+  addImage,
+  addVideo,
+  addStiker,
+  addAudio
+};
