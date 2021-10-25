29fa854597326797ff4881258d7b65050537091c
Ma'ruf <72728486+mrfzvx12@users.noreply.github.com>
2021-10-25 12:45:50 +0700

    Add files via upload

--- result/search/playstore.js
+++ result/search/playstore.js
@@ -1,1 +1,46 @@
-//
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+function playstore(name){
+	return new Promise((resolve, reject) => {
+		axios.get('https://play.google.com/store/search?q='+ name +'&c=apps')
+		.then(({ data }) => {
+			const $ = cheerio.load(data)
+			let ln = [];
+			let nm = [];
+			let dv = [];
+			let lm = [];
+			const result = [];
+			$('div.wXUyZd > a').each(function(a,b){
+				const link =  'https://play.google.com' + $(b).attr('href')
+				ln.push(link);
+			})
+			$('div.b8cIId.ReQCgd.Q9MA7b > a > div').each(function(d,e){
+				const name = $(e).text().trim()
+				nm.push(name);
+			})
+			$('div.b8cIId.ReQCgd.KoLSrc > a > div').each(function(f,g){
+				const dev = $(g).text().trim();
+				dv.push(dev)
+			})
+			$('div.b8cIId.ReQCgd.KoLSrc > a').each(function(h,i){
+				const limk = 'https://play.google.com' + $(i).attr('href');
+				lm.push(limk);
+			})			
+		for (let i = 0; i < ln.length; i++){
+			result.push({
+				name: nm[i],
+				link: ln[i],
+				developer: dv[i],
+				link_dev: lm[i]
+			})
+	}
+		resolve(result)
+		})
+	.catch(reject)
+	})
+}
+
+module.exports = {
+  playstore
+}
