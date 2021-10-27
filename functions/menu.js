6b1a77de3a4ce7b063f924518939715cc70a9dee
Ma'ruf <72728486+mrfzvx12@users.noreply.github.com>
2021-10-27 09:29:43 +0700

    Default menu

--- functions/menu.js
+++ functions/menu.js
@@ -1,135 +1,127 @@
-exports.menu = () => {
+exports.menu = (v) => {
   return `
-┏━⊷⟬*MAIN*⟭⊶
-┃➫ ${v}menu
-┃➫ ${v}help
-┃➫ ${v}info
-┃➫ ${v}bahasa
-┃➫ ${v}ping
-┗━━━━━━━━
-
-┏━⊷⟬*FUN*⟭⊶
-┃➬ ${v}dilan
-┃➬ ${v}ilham
-┃➬ ${v}gombal
-┃➬ ${v}hacker
-┃➬ ${v}fakta
-┃➬ ${v}fitnah text/target/text
-┗━━━━━━━━
-
-┏━⊷⟬*KERANG AJAIB*⟭⊶
-┃➯ ${v}apakah
-┃➯ ${v}kapankah
-┃➯ ${v}siapakah
-┗━━━━━━━━
-
-┏━⊷⟬*GAME*⟭⊶
-┃➣ ${v}truth
-┃➣ ${v}dare
-┃➣ ${v}asahotak
-┃➣ ${v}siapakahaku
-┃➣ ${v}tebakkalimat
-┃➣ ${v}tebakkata
-┃➣ ${v}tebaklirik
-┃➣ ${v}tekateki
-┗━━━━━━━━
-
-┏━⊷⟬*STICKER*⟭⊶
-┃➙ ${v}sticker
-┃➙ ${v}sticker <author>|<packname>
-┃➙ ${v}take <author>|<packname>
-┃➙ ${v}toimg
-┗━━━━━━━━
-
-┏━⊷⟬*PICTURE*⟭⊶
-┃➨ ${v}pinterest <query>
-┃➨ ${v}cogan
-┃➨ ${v}cecan
-┃➨ ${v}cyberpunk
-┃➨ ${v}wallpaper
-┗━━━━━━━━
-
-┏━⊷⟬*TOOLS*⟭⊶
-┃➥ ${v}fakethumb
-┃➥ ${v}tomp3
-┃➥ ${v}tovn
-┃➥ ${v}q
-┗━━━━━━━━
-
-┏━⊷⟬*INFORMASI*⟭⊶
-┃➥ ${v}covid
-┃➥ ${v}translate <lang><text>
-┗━━━━━━━━
-
-┏━⊷⟬*INTERNET*⟭⊶
-┃➯ ${v}playstore <query>
-┃➯ ${v}cerpen
-┗━━━━━━━━
-
-┏━⊷⟬*DOWNLOAD*⟭⊶
-┃ᐈ ${v}igdl <link>
-┃ᐈ ${v}igvid <link>
-┃ᐈ ${v}igimg <link>
-┃ᐈ ${v}reels <link>
-┃ᐈ ${v}tiktok <link>
-┃ᐈ ${v}tiktoknowm <link>
-┃ᐈ ${v}tiktokaudio <link>
-┗━━━━━━━━
-
-┏━⊷⟬*DATABASE*⟭⊶
-┃➠ ${v}addimg <text>
-┃➠ ${v}addvid <text>
-┃➠ ${v}addstik <text>
-┃➠ ${v}addvn <text>
-┃➠ ${v}getimg <text>
-┃➠ ${v}getvid <text>
-┃➠ ${v}getstik <text>
-┃➠ ${v}getvn <text>
-┃➠ ${v}listmedia
-┗━━━━━━━━
-
-┏━⊷⟬*ADMIN*⟭⊶
-┃➡️ ${v}open
-┃➡️ ${v}close
-┃➡️ ${v}link
-┃➡️ ${v}revoke
-┃➡️ ${v}online
-┃➡️ ${v}offline
-┃➡️ ${v}hidetag
-┃➡️ ${v}setppgc
-┃➡️ ${v}setname <text>
-┃➡️ ${v}setdesk <text>
-┃➡️ ${v}add <nomor>
-┃➡️ ${v}kick @user
-┃➡️ ${v}promote @user
-┃➡️ ${v}demote @user
-┃➡️ ${v}warn @user
-┃➡️ ${v}delwarn @user
-┃➡️ ${v}cekwarn @user
-┗━━━━━━━━
-
-┏━⊷⟬*GROUP*⟭⊶
-┃▶️ ${v}welcome on
-┃▶️ ${v}welcome off
-┃▶️ ${v}antidelete on
-┃▶️ ${v}antidelete off
-┃▶️ ${v}detect on
-┃▶️ ${v}detect off
-┃▶️ ${v}antilink on
-┃▶️ ${v}antilink off
-┗━━━━━━━━
-
-┏━⊷⟬*OWNER*⟭⊶
-┃↪️ ${v}join <link>
-┃↪️ ${v}addpremium @user
-┃↪️ ${v}delpremium @user
-┃↪️ ${v}banned @user
-┃↪️ ${v}unbanned @user
-┃↪️ ${v}upsw
-┃↪️ ${v}setppbot
-┃↪️ ${v}setthumb
-┃↪️ ${v}setfakethumb
-┃↪️ ${v}$
-┃↪️ ${v}>
-┗━━━━━━━━`;
+╭─⊷❲ *MAIN* ❳
+├ ${v}menu
+├ ${v}help
+├ ${v}info
+├ ${v}bahasa
+├ ${v}ping
+╰────────
+╭─⊷❲ *FUN* ❳
+├ ${v}dilan
+├ ${v}ilham
+├ ${v}gombal
+├ ${v}hacker
+├ ${v}fakta
+├ ${v}fitnah text/target/text
+╰────────
+╭─⊷❲ *KERANG AJAIB* ❳
+├ ${v}apakah
+├ ${v}kapankah
+├ ${v}siapakah
+╰────────
+╭─⊷❲ *GAME* ❳
+├ ${v}truth
+├ ${v}dare
+├ ${v}asahotak
+├ ${v}siapakahaku
+├ ${v}tebakkalimat
+├ ${v}tebakkata
+├ ${v}tebaklirik
+├ ${v}tekateki
+╰────────
+╭─⊷❲ *STICKER* ❳
+├ ${v}sticker
+├ ${v}sticker <author>|<packname>
+├ ${v}take <author>|<packname>
+├ ${v}toimg
+╰────────
+╭─⊷❲ *PICTURE* ❳
+├ ${v}pinterest <query>
+├ ${v}cogan
+├ ${v}cecan
+├ ${v}cyberpunk
+├ ${v}wallpaper
+╰────────
+╭─⊷❲ *TOOLS* ❳
+├ ${v}fakethumb
+├ ${v}tomp3
+├ ${v}tovn
+├ ${v}q
+╰────────
+╭─⊷❲ *INFORMASI* ❳
+├ ${v}covid
+├ ${v}translate <lang><text>
+╰────────
+╭─⊷❲ *INTERNET* ❳
+├ ${v}playstore <query>
+├ ${v}cerpen
+╰────────
+╭─⊷❲ *DOWNLOAD* ❳
+├ ${v}igdl <link>
+├ ${v}igvid <link>
+├ ${v}igimg <link>
+├ ${v}reels <link>
+├ ${v}tiktok <link>
+├ ${v}tiktoknowm <link>
+├ ${v}tiktokaudio <link>
+╰────────
+╭─⊷❲ *TEXT GENERATOR* ❳
+├ ${v}namaninja <text>
+├ ${v}typewriter <text>
+├ ${v}blackpill <text>
+╰────────
+╭─⊷❲ *DATABASE* ❳
+├ ${v}addimg <text>
+├ ${v}addvid <text>
+├ ${v}addstik <text>
+├ ${v}addvn <text>
+├ ${v}getimg <text>
+├ ${v}getvid <text>
+├ ${v}getstik <text>
+├ ${v}getvn <text>
+├ ${v}listmedia
+╰────────
+╭─⊷❲ *ADMIN* ❳
+├ ${v}open
+├ ${v}close
+├ ${v}link
+├ ${v}revoke
+├ ${v}online
+├ ${v}offline
+├ ${v}hidetag
+├ ${v}setppgc
+├ ${v}setname <text>
+├ ${v}setdesk <text>
+├ ${v}add <nomor>
+├ ${v}kick @user
+├ ${v}promote @user
+├ ${v}demote @user
+├ ${v}warn @user
+├ ${v}delwarn @user
+├ ${v}cekwarn @user
+╰────────
+╭─⊷❲ *GROUP* ❳
+├ ${v}welcome on
+├ ${v}welcome off
+├ ${v}antidelete on
+├ ${v}antidelete off
+├ ${v}detect on
+├ ${v}detect off
+├ ${v}antilink on
+├ ${v}antilink off
+╰────────
+╭─⊷❲ *OWNER* ❳
+├ ${v}join <link>
+├ ${v}addpremium @user
+├ ${v}delpremium @user
+├ ${v}banned @user
+├ ${v}unbanned @user
+├ ${v}upsw
+├ ${v}setppbot
+├ ${v}setthumb
+├ ${v}setfakethumb
+├ $
+├ >
+╰────────`;
 };
