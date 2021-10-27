19e46227f27731658685e01b040487c729d5e170
Hans <77654205+HansUan010101@users.noreply.github.com>
2021-10-27 08:30:56 +0700

    Kasih emot doang bg:v

--- functions/menu.js
+++ functions/menu.js
@@ -1,122 +1,135 @@
 exports.menu = () => {
   return `
-╭─⊷❲ *MAIN* ❳
-├ menu
-├ help
-├ info
-├ bahasa
-├ ping
-╰────────
-╭─⊷❲ *FUN* ❳
-├ dilan
-├ ilham
-├ gombal
-├ hacker
-├ fakta
-├ fitnah text/target/text
-╰────────
-╭─⊷❲ *KERANG AJAIB* ❳
-├ apakah
-├ kapankah
-├ siapakah
-╰────────
-╭─⊷❲ *GAME* ❳
-├ truth
-├ dare
-├ asahotak
-├ siapakahaku
-├ tebakkalimat
-├ tebakkata
-├ tebaklirik
-├ tekateki
-╰────────
-╭─⊷❲ *STICKER* ❳
-├ sticker
-├ sticker <author>|<packname>
-├ take <author>|<packname>
-├ toimg
-╰────────
-╭─⊷❲ *PICTURE* ❳
-├ pinterest <query>
-├ cogan
-├ cecan
-├ cyberpunk
-├ wallpaper
-╰────────
-╭─⊷❲ *TOOLS* ❳
-├ fakethumb
-├ tomp3
-├ tovn
-├ q
-╰────────
-╭─⊷❲ *INFORMASI* ❳
-├ covid
-├ translate <lang><text>
-╰────────
-╭─⊷❲ *INTERNET* ❳
-├ playstore <query>
-├ cerpen
-╰────────
-╭─⊷❲ *DOWNLOAD* ❳
-├ igdl <link>
-├ igvid <link>
-├ igimg <link>
-├ reels <link>
-├ tiktok <link>
-├ tiktoknowm <link>
-├ tiktokaudio <link>
-╰────────
-╭─⊷❲ *DATABASE* ❳
-├ addimg <text>
-├ addvid <text>
-├ addstik <text>
-├ addvn <text>
-├ getimg <text>
-├ getvid <text>
-├ getstik <text>
-├ getvn <text>
-├ listmedia
-╰────────
-╭─⊷❲ *ADMIN* ❳
-├ open
-├ close
-├ link
-├ revoke
-├ online
-├ offline
-├ hidetag
-├ setppgc
-├ setname <text>
-├ setdesk <text>
-├ add <nomor>
-├ kick @user
-├ promote @user
-├ demote @user
-├ warn @user
-├ delwarn @user
-├ cekwarn @user
-╰────────
-╭─⊷❲ *GROUP* ❳
-├ welcome on
-├ welcome off
-├ antidelete on
-├ antidelete off
-├ detect on
-├ detect off
-├ antilink on
-├ antilink off
-╰────────
-╭─⊷❲ *OWNER* ❳
-├ join <link>
-├ addpremium @user
-├ delpremium @user
-├ banned @user
-├ unbanned @user
-├ upsw
-├ setppbot
-├ setthumb
-├ setfakethumb
-├ $
-├ >
-╰────────`;
+┏━⊷⟬*MAIN*⟭⊶
+┃➫ ${v}menu
+┃➫ ${v}help
+┃➫ ${v}info
+┃➫ ${v}bahasa
+┃➫ ${v}ping
+┗━━━━━━━━
+
+┏━⊷⟬*FUN*⟭⊶
+┃➬ ${v}dilan
+┃➬ ${v}ilham
+┃➬ ${v}gombal
+┃➬ ${v}hacker
+┃➬ ${v}fakta
+┃➬ ${v}fitnah text/target/text
+┗━━━━━━━━
+
+┏━⊷⟬*KERANG AJAIB*⟭⊶
+┃➯ ${v}apakah
+┃➯ ${v}kapankah
+┃➯ ${v}siapakah
+┗━━━━━━━━
+
+┏━⊷⟬*GAME*⟭⊶
+┃➣ ${v}truth
+┃➣ ${v}dare
+┃➣ ${v}asahotak
+┃➣ ${v}siapakahaku
+┃➣ ${v}tebakkalimat
+┃➣ ${v}tebakkata
+┃➣ ${v}tebaklirik
+┃➣ ${v}tekateki
+┗━━━━━━━━
+
+┏━⊷⟬*STICKER*⟭⊶
+┃➙ ${v}sticker
+┃➙ ${v}sticker <author>|<packname>
+┃➙ ${v}take <author>|<packname>
+┃➙ ${v}toimg
+┗━━━━━━━━
+
+┏━⊷⟬*PICTURE*⟭⊶
+┃➨ ${v}pinterest <query>
+┃➨ ${v}cogan
+┃➨ ${v}cecan
+┃➨ ${v}cyberpunk
+┃➨ ${v}wallpaper
+┗━━━━━━━━
+
+┏━⊷⟬*TOOLS*⟭⊶
+┃➥ ${v}fakethumb
+┃➥ ${v}tomp3
+┃➥ ${v}tovn
+┃➥ ${v}q
+┗━━━━━━━━
+
+┏━⊷⟬*INFORMASI*⟭⊶
+┃➥ ${v}covid
+┃➥ ${v}translate <lang><text>
+┗━━━━━━━━
+
+┏━⊷⟬*INTERNET*⟭⊶
+┃➯ ${v}playstore <query>
+┃➯ ${v}cerpen
+┗━━━━━━━━
+
+┏━⊷⟬*DOWNLOAD*⟭⊶
+┃ᐈ ${v}igdl <link>
+┃ᐈ ${v}igvid <link>
+┃ᐈ ${v}igimg <link>
+┃ᐈ ${v}reels <link>
+┃ᐈ ${v}tiktok <link>
+┃ᐈ ${v}tiktoknowm <link>
+┃ᐈ ${v}tiktokaudio <link>
+┗━━━━━━━━
+
+┏━⊷⟬*DATABASE*⟭⊶
+┃➠ ${v}addimg <text>
+┃➠ ${v}addvid <text>
+┃➠ ${v}addstik <text>
+┃➠ ${v}addvn <text>
+┃➠ ${v}getimg <text>
+┃➠ ${v}getvid <text>
+┃➠ ${v}getstik <text>
+┃➠ ${v}getvn <text>
+┃➠ ${v}listmedia
+┗━━━━━━━━
+
+┏━⊷⟬*ADMIN*⟭⊶
+┃➡️ ${v}open
+┃➡️ ${v}close
+┃➡️ ${v}link
+┃➡️ ${v}revoke
+┃➡️ ${v}online
+┃➡️ ${v}offline
+┃➡️ ${v}hidetag
+┃➡️ ${v}setppgc
+┃➡️ ${v}setname <text>
+┃➡️ ${v}setdesk <text>
+┃➡️ ${v}add <nomor>
+┃➡️ ${v}kick @user
+┃➡️ ${v}promote @user
+┃➡️ ${v}demote @user
+┃➡️ ${v}warn @user
+┃➡️ ${v}delwarn @user
+┃➡️ ${v}cekwarn @user
+┗━━━━━━━━
+
+┏━⊷⟬*GROUP*⟭⊶
+┃▶️ ${v}welcome on
+┃▶️ ${v}welcome off
+┃▶️ ${v}antidelete on
+┃▶️ ${v}antidelete off
+┃▶️ ${v}detect on
+┃▶️ ${v}detect off
+┃▶️ ${v}antilink on
+┃▶️ ${v}antilink off
+┗━━━━━━━━
+
+┏━⊷⟬*OWNER*⟭⊶
+┃↪️ ${v}join <link>
+┃↪️ ${v}addpremium @user
+┃↪️ ${v}delpremium @user
+┃↪️ ${v}banned @user
+┃↪️ ${v}unbanned @user
+┃↪️ ${v}upsw
+┃↪️ ${v}setppbot
+┃↪️ ${v}setthumb
+┃↪️ ${v}setfakethumb
+┃↪️ ${v}$
+┃↪️ ${v}>
+┗━━━━━━━━`;
 };
