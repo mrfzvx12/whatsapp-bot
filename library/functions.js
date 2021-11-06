629ed5bb9cb6133a2721558cc2bf7a3e8f825c6c
Ma'ruf <72728486+mrfzvx12@users.noreply.github.com>
2021-11-06 10:12:06 +0700

    update functions.js

--- library/functions.js
+++ library/functions.js
@@ -17,6 +17,14 @@ const isUrl = (url) => {
       );
     };
 
+const isLinkyt = (url) => {
+       return url.match(
+        new RegExp(
+          /(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/,
+          "gi"
+        )
+      );
+    };
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)];
@@ -156,6 +164,7 @@ module.exports = {
   modStick,
   h2k,
   isUrl,
+  isLinkyt,
   pickRandom,
   generateMessageID, 
   getGroupAdmins, 
