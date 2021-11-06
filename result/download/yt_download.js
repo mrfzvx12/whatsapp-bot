06b3323c417afe4622a77a0c87dc145f33eab83b
Ma'ruf <72728486+mrfzvx12@users.noreply.github.com>
2021-11-06 08:18:55 +0700

    update yt_download.js

--- result/download/yt_download.js
+++ result/download/yt_download.js
@@ -1,11 +1,8 @@
-const fetch = require('node-fetch')
-const { JSDOM } = require('jsdom')
+const fetch = require('node-fetch') 
+const cheerio = require('cheerio')
 
-const ytIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
-
-
+const ytv = async (yutub) => {
 function post(url, formdata) {
-    Object.keys(formdata).map(key => `${key}=${encodeURIComponent(formdata[key])}`).join('&');
     return fetch(url, {
         method: 'POST',
         headers: {
@@ -13,97 +10,89 @@ function post(url, formdata) {
             'accept-language': "en-US,en;q=0.9",
             'content-type': "application/x-www-form-urlencoded; charset=UTF-8"
         },
-        body: Object.keys(formdata).map(key => `${key}=${encodeURIComponent(formdata[key])}`).join('&')
+        body: new URLSearchParams(Object.entries(formdata))
     })
 }
-
-function ytv(url) {
-    return new Promise((resolve, reject) => {
-        if (ytIdRegex.test(url)) {
-            let ytId = ytIdRegex.exec(url)
-            url = 'https://youtu.be/' + ytId[1]
-            post('https://www.y2mate.com/mates/en60/analyze/ajax', {
-                url,
-                q_auto: 0,
-                ajax: 1
-            })
-                .then(res => res.json())
-                .then(res => {
-                    document = (new JSDOM(res.result)).window.document
-                    yaha = document.querySelectorAll('td')
-                    filesize = yaha[yaha.length - 23].innerHTML
-                    id = /var k__id = "(.*?)"/.exec(document.body.innerHTML) || ['', '']
-                    thumb = document.querySelector('img').src
-                    title = document.querySelector('b').innerHTML
-
-                    post('https://www.y2mate.com/mates/en60/convert', {
-                        type: 'youtube',
-                        _id: id[1],
-                        v_id: ytId[1],
-                        ajax: '1',
-                        token: '',
-                        ftype: 'mp4',
-                        fquality: 360
-                    })
-                        .then(res => res.json())
-                        .then(res => {
-                            let KB = parseFloat(filesize) * (1000 * /MB$/.test(filesize))
-                            resolve({
-                                dl_link: /<a.+?href="(.+?)"/.exec(res.result)[1],
-                                thumb,
-                                title,
-                                filesizeF: filesize,
-                                filesize: KB
-                            })
-                        }).catch(reject)
-                }).catch(reject)
-        } else reject('URL INVALID')
-    })
-}
-
-function yta(url) {
-    return new Promise((resolve, reject) => {
-        if (ytIdRegex.test(url)) {
-            let ytId = ytIdRegex.exec(url)
-            url = 'https://youtu.be/' + ytId[1]
-            post('https://www.y2mate.com/mates/en60/analyze/ajax', {
-                url,
-                q_auto: 0,
-                ajax: 1
-            })
-                .then(res => res.json())
-                .then(res => {
-                    let document = (new JSDOM(res.result)).window.document
-                    let type = document.querySelectorAll('td')
-                    let filesize = type[type.length - 10].innerHTML
-                    let id = /var k__id = "(.*?)"/.exec(document.body.innerHTML) || ['', '']
-                    let thumb = document.querySelector('img').src
-                    let title = document.querySelector('b').innerHTML
-
-                    post('https://www.y2mate.com/mates/en60/convert', {
-                        type: 'youtube',
-                        _id: id[1],
-                        v_id: ytId[1],
-                        ajax: '1',
-                        token: '',
-                        ftype: 'mp3',
-                        fquality: 128
-                    })
-                        .then(res => res.json())
-                        .then(res => {
-                            let KB = parseFloat(filesize) * (1000 * /MB$/.test(filesize))
-                            resolve({
-                                dl_link: /<a.+?href="(.+?)"/.exec(res.result)[1],
-                                thumb,
-                                title,
-                                filesizeF: filesize,
-                                filesize: KB
-                            })
-                        }).catch(reject)
-                }).catch(reject)
-        } else reject('URL INVALID')
-    })
+const ytIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
+let ytId = ytIdRegex.exec(yutub)
+url = 'https://youtu.be/' + ytId[1]
+let res = await post(`https://www.y2mate.com/mates/en68/analyze/ajax`, {
+    url,
+    q_auto: 0,
+    ajax: 1
+  })
+const mela = await res.json()
+const $ = cheerio.load(mela.result)
+const hasil = []
+let thumb = $('div').find('.thumbnail.cover > a > img').attr('src')
+let judul = $('div').find('.thumbnail.cover > div > b').text()
+let quality = $('div').find('#mp4 > table > tbody > tr:nth-child(4) > td:nth-child(3) > a').attr('data-fquality')
+let tipe = $('div').find('#mp4 > table > tbody > tr:nth-child(3) > td:nth-child(3) > a').attr('data-ftype')
+let output = `${judul}.` + tipe
+let size = $('div').find('#mp4 > table > tbody > tr:nth-child(4) > td:nth-child(2)').text()
+let id = /var k__id = "(.*?)"/.exec(mela.result)[1]
+let res2 = await post(`https://www.y2mate.com/mates/en68/convert`, {
+    type: 'youtube',
+    _id: id,
+    v_id: ytId[1],
+    ajax: '1',
+    token: '',
+    ftype: tipe,
+    fquality: quality
+  })
+const meme = await res2.json()
+const supp = cheerio.load(meme.result)
+let link = supp('div').find('a').attr('href')
+hasil.push({ thumb, judul, quality, tipe, size, output, link})
+return hasil[0]
 }
 
 
-module.exports = { yta, ytv }
+const yta= async (yutub) => {
+function post(url, formdata) {
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            accept: "*/*",
+            'accept-language': "en-US,en;q=0.9",
+            'content-type': "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        body: new URLSearchParams(Object.entries(formdata))
+    })
+}
+const ytIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
+let ytId = ytIdRegex.exec(yutub)
+url = 'https://youtu.be/' + ytId[1]
+let res = await post(`https://www.y2mate.com/mates/en68/analyze/ajax`, {
+    url,
+    q_auto: 0,
+    ajax: 1
+  })
+const mela = await res.json()
+const $ = cheerio.load(mela.result)
+const hasil = []
+let thumb = $('div').find('.thumbnail.cover > a > img').attr('src')
+let judul = $('div').find('.thumbnail.cover > div > b').text()
+let size = $('div').find('#mp3 > table > tbody > tr > td:nth-child(2)').text()
+let tipe = $('div').find('#mp3 > table > tbody > tr > td:nth-child(3) > a').attr('data-ftype')
+let output = `${judul}.` + tipe
+let quality = $('div').find('#mp3 > table > tbody > tr > td:nth-child(3) > a').attr('data-fquality')
+let id = /var k__id = "(.*?)"/.exec(mela.result)[1]
+let res2 = await post(`https://www.y2mate.com/mates/en68/convert`, {
+    type: 'youtube',
+    _id: id,
+    v_id: ytId[1],
+    ajax: '1',
+    token: '',
+    ftype: tipe,
+    fquality: quality
+  })
+const meme = await res2.json()
+const supp = cheerio.load(meme.result)
+let link = supp('div').find('a').attr('href')
+hasil.push({ thumb, judul, quality, tipe, size, output, link})
+return hasil[0]
+}
+
+
+module.exports = { yta, ytv}
