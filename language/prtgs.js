0c4655fa4736e637d42aadc6f64559db2e920953
Ηͥ͢Ꭿͣ͜ℕͫ͜Ѕ͢×፝֟͜× <77654205+Hansalrl@users.noreply.github.com>
2021-11-18 20:12:13 +0700

    Anu
    Belum jadi lur

--- language/prtgs.js
+++ language/prtgs.js
@@ -1,1 +1,9 @@
-procurando o que??
+// -- send message
+exports.wait = `Por favor espere um momento...`;
+exports.bye = `Adeus...`;
+exports.done = `Finalizado...`;
+exports.next = (value) => {
+  return 'Clique próximo para '+value+' próximo';
+};
+exports.packon = 'O nome do pacote já está registrado no banco de dados do bot';
+exports.packoff = 'O nome do pacote de mídia não está registrado no banco de dados do bot';
