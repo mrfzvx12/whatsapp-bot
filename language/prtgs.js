28011696f1f49eb346d064b5839cc83efbe816af
Ηͥ͢Ꭿͣ͜ℕͫ͜Ѕ͢×፝֟͜× <77654205+Hansalrl@users.noreply.github.com>
2021-11-18 20:33:23 +0700

    Update prtgs.js

--- language/prtgs.js
+++ language/prtgs.js
@@ -7,3 +7,21 @@ exports.next = (value) => {
 };
 exports.packon = 'O nome do pacote já está registrado no banco de dados do bot';
 exports.packoff = 'O nome do pacote de mídia não está registrado no banco de dados do bot';
+exports.liston = 'Lista de mídia armazenada no banco de dados do bot';
+exports.getlist = 'Use o comando #getimg / #getvid / #getvn / #getstik para recuperar cada pacote de mídia\n*Exemplo*: #getimg lexa';
+exports.nolink = (value) => {
+  return 'Ligação não disponível! por favor insira o comando adicionando um ligação ' + value;
+};
+
+// -- Saying time
+exports.night = 'Boa noite';
+exports.evening = 'Boa tarde';
+exports.day = 'Boa tarde';
+exports.morning = 'Bom Dia';
+
+// -- message only
+exports.admin = `Este comando só pode ser executado pelo grupo admin! \n por favor, converse com o administrador para executar o comando`;
+exports.group = `Este comando só pode ser executado dentro do grupo`;
+exports.premium = `Este comando só pode ser usado por usuários prêmio`;
+exports.premdl = `Você não é um usuário prêmio, baixe você mesmo usando o ligação\n*ligação* : `;
+exports.oversize = 'O tamanho do arquivo excede o tamanho especificado, faça o download você mesmo via ligação debaixo\n*Ligação* : ';
