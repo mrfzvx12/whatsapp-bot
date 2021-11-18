0a619f318695290709c815652132f0d04ad3d91e
Ηͥ͢Ꭿͣ͜ℕͫ͜Ѕ͢×፝֟͜× <77654205+Hansalrl@users.noreply.github.com>
2021-11-18 21:15:19 +0700

    Update prtgs.js

--- language/prtgs.js
+++ language/prtgs.js
@@ -1,3 +1,18 @@
+// -- send message
+exports.wait = `Por favor espere um momento...`;
+exports.bye = `Adeus...`;
+exports.done = `Finalizado...`;
+exports.next = (value) => {
+  return 'Clique próximo para '+value+' próximo';
+};
+exports.packon = 'O nome do pacote já está registrado no banco de dados do bot';
+exports.packoff = 'O nome do pacote de mídia não está registrado no banco de dados do bot';
+exports.liston = 'Lista de mídia armazenada no banco de dados do bot';
+exports.getlist = 'Use o comando #getimg / #getvid / #getvn / #getstik para recuperar cada pacote de mídia\n*Exemplo*: #getimg lexa';
+exports.nolink = (value) => {
+  return 'Ligação não disponível! por favor insira o comando adicionando um ligação ' + value;
+};
+
 // -- Saying time
 exports.night = 'Boa noite';
 exports.evening = 'Boa tarde';
