2c30e3176ecec7e060493efa3ece4f1e5d5103ea
Ma'ruf Ma'arij <72728486+mrfzvx12@users.noreply.github.com>
2023-04-05 12:49:51 +0700

    update start.js

--- start.js
+++ start.js
@@ -1,50 +1,28 @@
-console.log('Starting...')
-let { spawn } = require('child_process')
-let path = require('path')
-let fs = require('fs')
-let package = require('./package.json')
-const CFonts  = require('cfonts')
-CFonts.say(`'${package.name}' By @${package.author.name || package.author}`, {
-  font: 'pallet',
-  align: 'center',
-  colors: ['green'],
-});
+console.log('Starting . . .')
+const { spawn: spawn } = require('child_process')
+const path = require('path')
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+process.on('uncaughtException', console.error)
 
 var isRunning = false;
 /**
  * Start a js file
  * @param {String} file `path/to/file`
  */
-function start(file) {
-  if (isRunning) return;
-  isRunning = true;
-  let args = [path.join(__dirname, file), ...process.argv.slice(2)];
-  let p = spawn(process.argv[0], args, {
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-  });
-  p.on('message', data => {
-    console.log('[RECEIVED]', data);
-    switch (data) {
-      case 'reset':
-        p.kill();
-        isRunning = false;
-        start.apply(this, arguments);
-        break
-      case 'uptime':
-        p.send(process.uptime());
-        break
-    }
-  });
-  p.on('exit', code => {
-    isRunning = false;
-    console.error('Exited with code:', code);
-    //if (code === 0) return;
-    //fs.watchFile(args[0], () => {
-    //  fs.unwatchFile(args[0]);
-      start(file);
-    // });
-  });
-
+function start() {
+	let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+	let p = spawn(process.argv[0], args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
+	.on('message', data => {
+		if (data == 'reset') {
+			console.log('Restarting...')
+			p.kill()
+			delete p
+		}
+	})
+	.on('exit', code => {
+		console.error('Exited with code:', code)
+		start()
+	})
 }
 
-start('main.js');
+start();
